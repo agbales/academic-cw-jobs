fda3e5e028e95989fe70f51755f7f67148633b5b
Andrew Bales <agbales@gmail.com>
2020-05-25 00:24:24 -0700

    Updates

---
+++ precache-manifest.ba2a8144b7a8adde43e9f3eec33d0286.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9e70515f2728e5f9e726b23cfc66dbc9",
+    "url": "/academic-cw-jobs/index.html"
+  },
+  {
+    "revision": "4c140f4dcf383301aaac",
+    "url": "/academic-cw-jobs/static/css/2.0e1a4c73.chunk.css"
+  },
+  {
+    "revision": "a0362e462ceecad2a77d",
+    "url": "/academic-cw-jobs/static/css/main.b310fb38.chunk.css"
+  },
+  {
+    "revision": "4c140f4dcf383301aaac",
+    "url": "/academic-cw-jobs/static/js/2.76180793.chunk.js"
+  },
+  {
+    "revision": "a0362e462ceecad2a77d",
+    "url": "/academic-cw-jobs/static/js/main.e4c6567e.chunk.js"
+  },
+  {
+    "revision": "579f84bdaada6e5f1679",
+    "url": "/academic-cw-jobs/static/js/runtime~main.53b87db8.js"
+  }
+]);
